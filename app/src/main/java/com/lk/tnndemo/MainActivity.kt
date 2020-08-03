fa6cfcd17b393cc843d10e494849b47b43d5a391
liork <liorkad3@gmail.com>
2020-08-03 14:04:29 +0300

    Add layout for app

--- app/src/main/java/com/lk/tnndemo/MainActivity.kt
+++ app/src/main/java/com/lk/tnndemo/MainActivity.kt
@@ -1,18 +1,49 @@
 package com.lk.tnndemo
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private var testBitmap: Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        System.loadLibrary("demo")
+        initModel()
+        initViews()
+    }
+
+    private fun initViews() {
+        try {
+            testBitmap = readBitmapFromFile(assets, "test.jpg")
+            ivTestImage.setImageBitmap(testBitmap)
+        }catch (e: IOException){
+            Toast.makeText(this, "error loading image", Toast.LENGTH_LONG).show()
+        }
+        btnDetect.setOnClickListener {  }
+    }
+
+    @Throws(IOException::class)
+    private fun initModel(): String {
+        val targetDir: String = filesDir.absolutePath
+
+        //copy detect model to sdcard
+        val modelPathsDetector = arrayOf(
+            "version-slim-320_simplified.tnnmodel",
+            "version-slim-320_simplified.tnnproto"
+        )
+        for (modelPath in modelPathsDetector) {
+            val interModelFilePath = "$targetDir/$modelPath"
+           copyAsset(assets, modelPath, interModelFilePath)
+        }
+        return targetDir
     }
 
 }
---
+++ app/src/main/java/com/lk/tnndemo/Utils.kt
@@ -0,0 +1,43 @@
+package com.lk.tnndemo
+
+import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.*
+
+@Throws(IOException::class)
+fun copyAsset(assetManager: AssetManager,
+               fromAssetPath: String, toPath: String){
+    var inputStream: InputStream? = null
+    var outputStream: OutputStream? = null
+    try {
+        inputStream = assetManager.open(fromAssetPath)
+        File(toPath).createNewFile()
+        outputStream = FileOutputStream(toPath)
+        copyFile(inputStream, outputStream)
+        outputStream.flush()
+    }finally {
+        inputStream?.close()
+        outputStream?.close()
+    }
+}
+
+@Throws(IOException::class)
+fun copyFile(inputStream: InputStream, outputStream: OutputStream) {
+    val buffer = ByteArray(1024)
+    var read: Int
+    while (inputStream.read(buffer).also { read = it } != -1) {
+        outputStream.write(buffer, 0, read)
+    }
+}
+@Throws(IOException::class)
+fun readBitmapFromFile(assetManager: AssetManager, filePath: String): Bitmap {
+    var inputStream: InputStream? = null
+    try {
+        inputStream = assetManager.open(filePath)
+    }finally {
+        inputStream?.close()
+    }
+    return BitmapFactory.decodeStream(inputStream)
+}
+
